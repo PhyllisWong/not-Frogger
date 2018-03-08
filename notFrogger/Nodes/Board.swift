ae8506cb2490c67a644145f617d38de7200177b2
Phyllis <wong.phyllism@gmail.com>
2018-03-08 14:00:54 -0800

    add Board class

--- notFrogger/Nodes/Board.swift
+++ notFrogger/Nodes/Board.swift
@@ -38,14 +38,44 @@ class Board: SKNode {
     ///
     /// - Paremeter size: The size of the board which determiens the size of the stripes.
 
-    /* init(size: CGSize) {
-        super.inti()
+    init(size: CGSize) {
+        super.init()
         self.size = size
         self.name = "board"
         make()
-        
-    } */
+    }
     
+    // Required to make our custome class work
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension Board {
+    
+    /// Create the stripes for the board and places them in a grid layout.
+    func make() {
+        stripes = Stripes()
+        
+        // Make rows
+        for row in -1 ... 1 {
+            let stripe = Stripe(size: CGSize(width: size.width, height: 60), orientation: .horizontal)
+            addChild(stripe)
+            stripes.horizontal.append(stripe)
+            stripe.position.x = size.width / 2
+            stripe.position.y = size.height / 2 - CGFloat(65 * row)
+            
+        }
+
+        // Make columns
+        for column in -1 ... 1 {
+            let stripe = Stripe(size: CGSize(width: 60, height: size.height), orientation: .vertical)
+            addChild(stripe)
+            stripes.vertical.append(stripe)
+            stripe.position.x = size.width / 2  - CGFloat(65 * column)
+            stripe.position.y = size.height / 2
+        }
+    }
 }
 
 
